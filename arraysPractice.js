b689742e00f268dcfb7c6a1538341210f708259c
Kyle Prestgard <ktprestgard@gmail.com>
2016-01-21 18:47:34 -0700

    more updates for arrays

--- arraysPractice.js
+++ arraysPractice.js
@@ -67,12 +67,13 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
   //Code Here
 function evenFinder(evens) {
-  for (var i = 0; i < 0; i++) {
+  var fin = [];
+  for (var i = 0; i < evens.length; i++) {
     if (evens[i]%2===0) {
-      evens.splice(i, 1);
+      fin.push(evens[i]);
     }
   }
-  return evens;
+  return fin;
 }
 evenFinder(nums);
 //Next problem
@@ -122,22 +123,32 @@ var str = 'this is my sentence';
 
 var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 /*
-  Here we're going to write a function that mimics going shopping and checking things off of our grocery list,
-  and adding new items to our list. 
+  Here we're going to write a function that mimics going shopping and 
+  checking things off of our grocery list,and adding new items to our list. 
   
-  Write a function called removeItem that is given two arguments, the first is myGroceryList, and the
-  second is an item to remove from myGroceryList. If the second argument (or the item to add or remove) matches an item in myGroceryList,
-  remove that item from the your grocery list and return the new, updated grocery list. 
+  Write a function called removeItem that is given two arguments, the first 
+  is myGroceryList, and the second is an item to remove from myGroceryList.
+  If the second argument (or the item to add or remove) matches an item in 
+  myGroceryList, remove that item from the your grocery list and return the
+  new, updated grocery list. 
   
-  Once you do that, write another function called addItem that is given two arguments, the first is myGroceryList
-  and the second is an item to add to your grocery list. In addItem add the item you passed in to
-  myGroceryList then return the new, updated grocery list.
+  Once you do that, write another function called addItem that is given two 
+  arguments, the first is myGroceryList and the second is an item to add to 
+  your grocery list. In addItem add the item you passed in to myGroceryList 
+  then return the new, updated grocery list.
 */
 
   //Code Here
-
-//removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
-//addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
+  function removeItem(list, item) {
+    for (var i = 0; i <list.length; i++) {
+      if (item === list[i]) {
+        list.splice(i, 1)
+    }
+  }
+    return list;
+}
+removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
+addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
 
 
 
